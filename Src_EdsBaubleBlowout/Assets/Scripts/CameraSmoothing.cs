502d59c8248b14f9bab8274a06c9797a4a040795
Tobyshull <T.SMITH-2022@hull.ac.uk>
2022-11-26 12:09:46 +0000

    Implemented First Player Movement

---
+++ Src_EdsBaubleBlowout/Assets/Scripts/CameraSmoothing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothing : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float smoothing;
+
+    private Vector3 currentVel;
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVel, smoothing);
+    }
+}
---
+++ Src_EdsBaubleBlowout/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float sprintAdditional;
+
+    [SerializeField]
+    private float movementSmoothing;
+
+    [SerializeField]
+    private float boostSpeed;
+
+    [SerializeField]
+    private float boostRefreshTime;
+
+    [SerializeField]
+    private float movementResetTime;
+
+    [SerializeField]
+    private int boostsLeft;
+
+    private Rigidbody2D rb;
+    private Vector2 currentVel;
+
+    private int maxBoosts;
+    private float lastBoost;
+    private float lastBoostRefresh;
+
+    Vector2 movementDir;
+
+    bool sprint;
+    bool boost;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        maxBoosts = boostsLeft;
+        lastBoostRefresh = Time.time;
+    }
+
+    private void Update()
+    {
+        movementDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        sprint = Input.GetKey(KeyCode.LeftShift);
+
+        if(!boost)
+            boost = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Time.time - lastBoost > movementResetTime) {
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, movementDir * (sprint ? speed + sprintAdditional : speed), ref currentVel, movementSmoothing);
+        }
+
+        if (boost)
+        {
+            if (boostsLeft > 0)
+            {
+                boostsLeft--;
+                lastBoost = Time.time;
+                lastBoostRefresh = Time.time;
+                boost = false;
+
+                rb.AddForce(movementDir * boostSpeed);
+            }
+        }
+
+        if (boostsLeft < maxBoosts) {
+            if (Time.time - lastBoostRefresh > boostRefreshTime)
+            {
+                boostsLeft++;
+                lastBoostRefresh = Time.time;
+            }
+        }
+    }
+}
